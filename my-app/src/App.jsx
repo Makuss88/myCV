a01b4a2c122e4b73759d6fd1bda037fe874bd725
Makuss88 <maciborek.grzegorz@gmail.com>
2023-09-07 15:33:16 +0200

    make first styled

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,17 +1,23 @@
+import styled from "styled-components";
 import { Contact } from "./components/Contact";
 import { Hero } from "./components/Hero";
 import { Who } from "./components/Who";
 import { Works } from "./components/Works";
 
-function App() {
+const Container = styled.div`
+  height: 100vh;
+  background: aqua;
+`;
+
+const App = () => {
   return (
-    <div>
+    <Container style={{ color: "green" }}>
       <Hero />
       <Who />
       <Works />
       <Contact />
-    </div>
+    </Container>
   );
-}
+};
 
 export default App;
